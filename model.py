6d6d838b4926242e73994b35ff5ff8ada921c863
Hanhai <1932232849@qq.com>
2026-08-20 16:57:54 +0800

    Step 5: row_sum

--- model.py
+++ model.py
@@ -63,13 +63,10 @@ __global__ void row_sum(const float *matrix, float *out, int rows, int cols) {
     int r = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (r < rows) {
-        float sum = 0.0;
-
+        out[r] = 0.0;
         for (int c = 0; c < cols; c++) {
-            sum += matrix[r * cols + c];
+            out[r] += matrix[r * cols + c];
         }
-
-        out[r] = sum;
     }
 }
 
